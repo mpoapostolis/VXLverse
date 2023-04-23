beaa8339c4b52b0900a875378f823c5eab21b7ee
mpoapostolis <mpoapostolis@gmail.com>
2023-04-23 17:27:09 +0300

    privacy policy

---
+++ pages/policy.tsx
@@ -0,0 +1,49 @@
+// components/PrivacyPolicy.tsx
+
+export default function Page() {
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
+      <p>
+        VXLverse ("us", "we", or "our") operates the VXLverse mobile application (hereinafter referred to as the
+        "Service").
+      </p>
+      <p>
+        This page informs you of our policies regarding the collection, use, and disclosure of personal data when you
+        use our Service and the choices you have associated with that data.
+      </p>
+      <p>By using the Service, you agree to the collection and use of information in accordance with this policy.</p>
+      <h2 className="text-xl font-semibold mt-4 mb-2">Information Collection and Use</h2>
+      <p>
+        While using our Service, we may ask you to provide us with certain personally identifiable information that can
+        be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is
+        not limited to:
+      </p>
+      <ul className="list-disc ml-6">
+        <li>Email address</li>
+        <li>First name and last name</li>
+        <li>Cookies and usage data</li>
+      </ul>
+      <h2 className="text-xl font-semibold mt-4 mb-2">Usage Data</h2>
+      <p>
+        When you access the Service, we may collect certain information automatically, including, but not limited to,
+        the type of device you use, your device unique ID, the IP address of your device, your operating system, and
+        other diagnostic data ("Usage Data").
+      </p>
+      <h2 className="text-xl font-semibold mt-4 mb-2">Cookies</h2>
+      <p>
+        We use cookies and similar tracking technologies to track the activity on our Service and hold certain
+        information. Cookies are files with small amounts of data, which may include an anonymous unique identifier.
+      </p>
+      <h2 className="text-xl font-semibold mt-4 mb-2">Changes to This Privacy Policy</h2>
+      <p>
+        We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy
+        Policy on this page. You are advised to review this Privacy Policy periodically for any changes.
+      </p>
+      <h2 className="text-xl font-semibold mt-4 mb-2">Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, please contact us by email at [Your Contact Email Address].
+      </p>
+    </div>
+  )
+}
