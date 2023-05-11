cd1de34ecaf7e3552b35137148533bb6ff0a1844
mpoapostolis <mpoapostolis@gmail.com>
2023-05-12 00:24:44 +0300

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,6 @@ import { EditorCanvas } from '@/components/canvas/editor'
 import Editor from '@/components/editor'
 import { Menu } from '@/components/menu'
 import { cn } from '@/lib/utils'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'VXLverse',
-  description:
-    'VXLverse is a revolutionary online platform designed to empower users to both create and play a wide variety of RPG games. Our mission is to provide an interactive and engaging space for game enthusiasts, offering them the opportunity to showcase their creativity and be rewarded for it.',
-  keywords: 'Game Development, RPG, VXLverse, 3D Modeling, Game Design, IndieDev, Gaming',
-}
 
 export default function Page() {
   return (
