151fc1ed6b2e4916c337584bd2f43532bb659d86
mpoapostolis <mpoapostolis@gmail.com>
2023-05-16 21:14:05 +0300

    ...

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ module.exports =
   process.env.NODE_ENV === 'development'
     ? {
         reactStrictMode: true,
-        productionBrowserSourceMaps: true,
-        experimental: {
-          legacyBrowsers: false,
-        },
       }
     : withPWA({
         // Next.js config
