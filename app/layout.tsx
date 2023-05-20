87a40f536ae82c2026d867d4c0c80c870cb1315c
mpoapostolis <mpoapostolis@gmail.com>
2023-05-20 11:36:15 +0300

    fix opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import { Toaster } from '@/components/ui/toaster'
 import '@/styles/globals.css'
 import { Metadata } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const APP_NAME = 'VXLverse'
 const APP_DEFAULT_TITLE = 'VXLverse'
 const APP_TITLE_TEMPLATE = 'VXLverse'
+const APP_CONTENT = '/logo-op.png'
 const APP_DESCRIPTION = `Imagine you're playing with your favorite toy set, 
 placing characters around, and creating fun stories with them. 
 That's exactly what VXLverse does, but in a digital world.`
@@ -14,13 +16,12 @@ That's exactly what VXLverse does, but in a digital world.`
 export const metadata: Metadata = {
   applicationName: APP_NAME,
   keywords: 'Game Development, RPG, VXLverse, 3D Modeling, Game Design, IndieDev, Gaming',
-
   title: {
     default: APP_DEFAULT_TITLE,
     template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
-  themeColor: '#FFFFFF',
+  themeColor: '#000000',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
@@ -32,13 +33,16 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     siteName: APP_NAME,
+    images: APP_CONTENT,
     title: {
       default: APP_DEFAULT_TITLE,
       template: APP_TITLE_TEMPLATE,
     },
     description: APP_DESCRIPTION,
   },
+
   twitter: {
+    images: APP_CONTENT,
     card: 'summary',
     title: {
       default: APP_DEFAULT_TITLE,
@@ -54,6 +58,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Link rel="icon" href="/favicon.ico" />
       </Head>
 
       <body className="overflow-hidden w-screen h-screen">
